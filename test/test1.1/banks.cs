2a4dc77306c80c91fb08bfc4c35fec5d52d32a80
HieuWhite <89020784+HieuWhite@users.noreply.github.com>
2025-08-11 14:57:49 +0700

    Create banks.cs

---
+++ test/test1.1/banks.cs
@@ -0,0 +1,16 @@
+#Test///
+
+bank:
+
+"""VPBankMBBank,HDBankMasan,"""
+
+emails:
+"""abc@vpbank.com.vn
+abd@mbbank.com.vn
+abe@hdbank.com.vn
+Masangroupiii,"""
+
+API_key: "xxxxxxxxxxxxxxxxxx;"
+Secret{}: "xxxxxxxxxxxxxxxxx;"
+
+
